f46feb9847f44f180486b6113587c4bcbc59d52e
EnragedStrings <85590396+EnragedStrings@users.noreply.github.com>
2022-11-13 16:13:31 -0600

    Add files via upload

---
+++ shapes.lua
@@ -0,0 +1,118 @@
+--Cube
+vertice.A = {1, 1, 1}
+vertice.B = {-1, 1, 1}
+vertice.C = {-1, -1, 1}
+vertice.D = {1, -1, 1}
+vertice.E = {1, 1, -1}
+vertice.F = {-1, 1, -1}
+vertice.G = {-1, -1, -1}
+vertice.H = {1, -1, -1}
+
+edge.AB = {"A", "B"}
+edge.BC = {"B", "C"}
+edge.CD = {"C", "D"}
+edge.DA = {"D", "A"}
+edge.EF = {"E", "F"}
+edge.FG = {"F", "G"}
+edge.GH = {"G", "H"}
+edge.HE = {"H", "E"}
+edge.AE = {"A", "E"}
+edge.BF = {"B", "F"}
+edge.CG = {"C", "G"}
+edge.DH = {"D", "H"}
+
+--Box
+vertice.A = {1, 1.5, 1}
+vertice.B = {-1, 1.5, 1}
+vertice.C = {-1, -1.5, 1}
+vertice.D = {1, -1.5, 1}
+vertice.E = {1, 1.5, -1}
+vertice.F = {-1, 1.5, -1}
+vertice.G = {-1, -1.5, -1}
+vertice.H = {1, -1.5, -1}
+
+edge.AB = {"A", "B"}
+edge.BC = {"B", "C"}
+edge.CD = {"C", "D"}
+edge.DA = {"D", "A"}
+edge.EF = {"E", "F"}
+edge.FG = {"F", "G"}
+edge.GH = {"G", "H"}
+edge.HE = {"H", "E"}
+edge.AE = {"A", "E"}
+edge.BF = {"B", "F"}
+edge.CG = {"C", "G"}
+edge.DH = {"D", "H"}
+
+--Hypercube
+vertice.A = {1, 1, 1}
+vertice.B = {-1, 1, 1}
+vertice.C = {-1, -1, 1}
+vertice.D = {1, -1, 1}
+vertice.E = {1, 1, -1}
+vertice.F = {-1, 1, -1}
+vertice.G = {-1, -1, -1}
+vertice.H = {1, -1, -1}
+
+vertice.I = {0.5, 0.5, 0.5}
+vertice.J = {-0.5, 0.5, 0.5}
+vertice.K = {-0.5, -0.5, 0.5}
+vertice.L = {0.5, -0.5, 0.5}
+vertice.M = {0.5, 0.5, -0.5}
+vertice.N = {-0.5, 0.5, -0.5}
+vertice.O = {-0.5, -0.5, -0.5}
+vertice.P = {0.5, -0.5, -0.5}
+
+edge.AB = {"A", "B"}
+edge.BC = {"B", "C"}
+edge.CD = {"C", "D"}
+edge.DA = {"D", "A"}
+edge.EF = {"E", "F"}
+edge.FG = {"F", "G"}
+edge.GH = {"G", "H"}
+edge.HE = {"H", "E"}
+edge.AE = {"A", "E"}
+edge.BF = {"B", "F"}
+edge.CG = {"C", "G"}
+edge.DH = {"D", "H"}
+
+edge.IJ = {"I", "J"}
+edge.JK = {"J", "K"}
+edge.KL = {"K", "L"}
+edge.LI = {"L", "I"}
+edge.MN = {"M", "N"}
+edge.NO = {"N", "O"}
+edge.OP = {"O", "P"}
+edge.PM = {"P", "M"}
+edge.IM = {"I", "M"}
+edge.JN = {"J", "N"}
+edge.KO = {"K", "O"}
+edge.LP = {"L", "P"}
+
+edge.AI = {"A", "I"}
+edge.BJ = {"B", "J"}
+edge.CK = {"C", "K"}
+edge.DL = {"D", "L"}
+edge.EM = {"E", "M"}
+edge.FN = {"F", "N"}
+edge.GO = {"G", "O"}
+edge.HP = {"H", "P"}
+
+--Pyramid
+vertice.A = {1, -1, 1}
+vertice.B = {1, -1, -1}
+vertice.C = {-1, -1, -1}
+vertice.D = {-1, -1, 1}
+vertice.E = {0, 0, 0}
+
+edge.AB = {"A", "B"}
+edge.BC = {"B", "C"}
+edge.CD = {"C", "D"}
+edge.DA = {"D", "A"}
+edge.AE = {"A", "E"}
+edge.BE = {"B", "E"}
+edge.CE = {"C", "E"}
+edge.DE = {"D", "E"}
+
+--Hell
+
